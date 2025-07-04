0c064c22d91397c24ca5c5a608e847c734b875e6
SFaceNWB <113598232+SFaceNWB@users.noreply.github.com>
2025-07-04 17:53:18 +0800

    交互功能输入

--- Plugins/Inventory/Source/Inventory/Private/Player/Bag_PlayerController.cpp
+++ Plugins/Inventory/Source/Inventory/Private/Player/Bag_PlayerController.cpp
@@ -2,13 +2,32 @@
 
 
 #include "Player/Bag_PlayerController.h"
-#include "Inventory.h"
 
+#include "EnhancedInputComponent.h"
+#include "EnhancedInputSubsystems.h"
 
 void ABag_PlayerController::BeginPlay()
 {
 	Super::BeginPlay();
 
-	UE_LOG(LogInventory, Log, TEXT("BeginPlay for PlayerController"));
+	UEnhancedInputLocalPlayerSubsystem* Subsystem = ULocalPlayer::GetSubsystem<UEnhancedInputLocalPlayerSubsystem>(GetLocalPlayer());
+	if (IsValid(Subsystem))
+	{
+		Subsystem->AddMappingContext(DefaultIMC, 0);
+	}
 
+}
+
+void ABag_PlayerController::SetupInputComponent()
+{
+	Super::SetupInputComponent();
+
+	UEnhancedInputComponent* EnhancedInputComponent = Cast<UEnhancedInputComponent>(InputComponent);
+
+	EnhancedInputComponent->BindAction(PrimaryInteractAction, ETriggerEvent::Started, this, &ABag_PlayerController::PrimaryInteract);
+}
+
+void ABag_PlayerController::PrimaryInteract()
+{
+	UE_LOG(LogTemp, Log, TEXT("Primary Interact"));
 }
